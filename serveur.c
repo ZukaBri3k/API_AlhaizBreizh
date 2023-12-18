2eb85ebab6ff5003cfbf5accf60686ad5937f036
DUMERCHAT KYRILL <kdumerchat@mx21>
2023-12-18 13:46:19 +0100

    debut script serveur

--- serveur.c
+++ serveur.c
@@ -7,6 +7,9 @@
 #include <unistd.h>
 #include <string.h>
 
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
 int main() {
 
     int sock;
@@ -19,7 +22,7 @@ int main() {
     sock = socket(AF_INET, SOCK_STREAM, 0);
     addr.sin_addr.s_addr = inet_addr("0.0.0.0");
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(8080);
+    addr.sin_port = htons(PORT);
     ret = bind(sock, (struct sockaddr *)&addr, sizeof(addr));
     printf("bind=%d\n", ret);
     ret = listen(sock, 1);
@@ -27,5 +30,9 @@ int main() {
     size = sizeof(conn_addr);
     cnx = accept(sock, (struct sockaddr *)&conn_addr, (socklen_t *)&size);
     printf("accept=%d\n", ret);
+
+    char buffer[BUFFER_SIZE];
+    int res = read(cnx, buffer, BUFFER_SIZE);
+    printf("read=%d, msg=%s\n", res, buffer);
 
 }
