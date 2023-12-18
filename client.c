f4a38e8ca65b15f8645bae5034d89d96c060c91f
MathisLB <mathis.leboite@gmail.com>
2023-12-18 15:08:22 +0100

    oskour

--- client.c
+++ client.c
@@ -7,6 +7,27 @@
 #include <unistd.h>
 #include <string.h>
 
+void readReadmeFile() {
+    FILE *file;
+    char ch;
+
+    // Ouvrir le fichier README en mode lecture
+    file = fopen("README.md", "r");
+
+    if (file == NULL) {
+        perror("Erreur lors de l'ouverture du fichier README.md");
+        exit(EXIT_FAILURE);
+    }
+
+    // Lire et afficher le contenu du fichier
+    while ((ch = fgetc(file)) != EOF) {
+        putchar(ch);
+    }
+
+    // Fermer le fichier
+    fclose(file);
+}
+
 int main() {
     int sock;
     int ret;
@@ -18,3 +39,5 @@ int main() {
     addr.sin_port = htons(8080);
     cnx = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
 }
+
+readReadmeFile();
