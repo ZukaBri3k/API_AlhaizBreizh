73f2cf01601e554bff81104596dd120bc1c7d667
DUMERCHAT KYRILL <kdumerchat@mx21>
2023-12-18 11:13:31 +0100

    debut prog serveur

--- serveur.c
+++ serveur.c
@@ -8,7 +8,7 @@
 #include <string.h>
 
 int main() {
-    
+
     int sock;
     int ret;
     struct sockaddr_in addr;
@@ -17,7 +17,7 @@ int main() {
     struct sockaddr_in conn_addr;
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_addr.s_addr = inet_addr("site-sae-ubisoufte.bigpapoo.com");
     addr.sin_family = AF_INET;
     addr.sin_port = htons(8080);
     ret = bind(sock, (struct sockaddr *)&addr, sizeof(addr));
