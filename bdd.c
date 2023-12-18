b29ed8cad16d26f68dc7a55aa814d5353836223d
EwanP56 <ewanpenven@gmail.com>
2023-12-18 14:20:15 +0100

    test appel bdd en c

---
+++ bdd.c
@@ -0,0 +1,21 @@
+#include <libpq-fe.h>
+#include <stdio.h>
+
+int main() {
+    const char *conninfo = "host=localhost port=5432 dbname=sae user=postgres password=vae4ua9phuch4Cef";
+    PGconn *conn = PQconnectdb(conninfo);
+    
+    if (PQstatus(conn) != CONNECTION_OK) {
+        fprintf(stderr, "Erreur lors de la connexion à la base de données : %s\n", PQerrorMessage(conn));
+        PQfinish(conn);
+        return 1;
+    }
+
+    PGresult *res = PQexec(conn, "SELECT id FROM personnes");
+    printf("Nombre de lignes : %d\n", PQntuples(res));
+
+    PQclear(res);
+    PQfinish(conn);
+
+    return 0;
+}
