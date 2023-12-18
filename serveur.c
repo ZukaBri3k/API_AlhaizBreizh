b7ade1df29f013ec52b22e57688a12348c99a5f7
DUMERCHAT KYRILL <kdumerchat@mx21>
2023-12-18 13:59:26 +0100

    debut script serveur

--- serveur.c
+++ serveur.c
@@ -32,7 +32,13 @@ int main() {
     printf("accept=%d\n", ret);
 
     char buffer[BUFFER_SIZE];
-    int res = read(cnx, buffer, BUFFER_SIZE-1);
-    printf("read=%d, msg=%s\n", res, buffer);
+    int res;
+
+    do
+    {
+        res = read(cnx, buffer, BUFFER_SIZE-1);
+        printf("read=%d, msg=%s\n", res, buffer);
+    } while (strcmp(buffer, "exit") != 0);
+    printf("close\n");
 
 }
