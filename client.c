1988c48604cc8df9a8895b5dbd43a131c3b35cfe
MathisLB <mathis.leboite@gmail.com>
2024-01-23 09:30:29 +0100

    Retour en arrière après tests

--- client.c
+++ client.c
@@ -8,59 +8,47 @@
 #include <string.h>
 #include <netdb.h>
 #include <errno.h>
-
 #define PORT 8000
-
 int main() {
     int sock;
     int ret;
     struct sockaddr_in addr;
-
         sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
-
     char *hostname = "site-sae-ubisoufte.bigpapoo.com";
     struct hostent *host = gethostbyname(hostname);
-
     if (host == NULL) {
         perror("gethostbyname");
         exit(EXIT_FAILURE);
     }
-
     memcpy(&addr.sin_addr, host->h_addr_list[0], host->h_length);
-
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
-
     ret = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
     if (ret == -1) {
-        fprintf(stderr, "connect: %s", strerror(errno));
+        fprintf(stderr, "connect: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
-
     char buffer[1024];
     char response[1024];
     read(sock, buffer, sizeof(buffer));
-    printf("%s", buffer);
+    printf("%s\n", buffer);
     scanf("%s", response);
     send(sock, response, strlen(response), 0);
-    read(sock, buffer, sizeof(buffer));
-    printf("%s", buffer);
+    read(sock, buffer, strlen(buffer));
+    printf("%s\n", buffer);
     int choix;
     choix=1;
     do {
         printf("Que souhaitez-vous faire ?\n");
         printf("1. Consulter la liste des biens\n");
-        printf("2. Consulter la disponibilité d'un bien\n");
-        printf("3. Modifier les dates d'indisponibilité d'un bien\n");
         printf("4. Consulter la liste de tous les biens (admin)\n");
         printf("0. Quitter\n");
         printf("Votre choix : ");
         scanf("%d", &choix);
-        
         //if (choix == 1) {
         //    send(sock, &command, sizeof(command), 0);
         //    recv(sock, &response, sizeof(response), 0);
@@ -78,8 +66,8 @@ int main() {
         //    printf("Dates d'indisponibilité modifiées avec succès.\n");
         //    printf(response);
         /*}else*/if (choix == 4) {
-            send(sock, "getLogement", sizeof("getLogement"), 0);
-            recv(sock, response, sizeof(response), 0);
+            send(sock, "getLogement", strlen("getLogement"), 0);
+            recv(sock, response, strlen(response), 0);
             printf("La liste de tous les biens :\n");
             printf(response);
         } else if (choix == 0) {
