5fc6864cbc651053e76084ef21c7ce623cbbb4d0
MathisLB <mathis.leboite@gmail.com>
2024-01-23 15:24:44 +0100

    Fix typo in client.c

--- client.c
+++ client.c
@@ -79,7 +79,7 @@ int main() {
         /*}else*/
         if (choix == 4) {
             printf("client : Choix 4\n");
-            int res = write(sock, "getLogements\r\n\0", sizeof("getLogements\r\n\0"));
+            int res = write(sock, "getLogement\r\n\0", sizeof("getLogement\r\n\0"));
             printf("client : res = %d\n", res);
             len = read(sock, buffer, sizeof(buffer));
             buffer[len] = '\0';
