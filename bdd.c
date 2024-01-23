41ce0cf993222ce477a6d51e1a5a111e86793239
EwanP56 <ewanpenven@gmail.com>
2024-01-23 09:59:45 +0100

    réparation du miseIndispo

--- bdd.c
+++ bdd.c
@@ -474,7 +474,7 @@ int miseIndispo(char cle[15], int cnx, char dateDebut[12], char dateFin[12]) {
         printf("%s\n", PQgetvalue(date_Debut, 1, 0));
         printf("%s\n", PQgetvalue(date_Debut, 2, 0));
         printf("%s\n", dateFin);
-        while (i < rows && strcmp(PQgetvalue(date_Debut, i, 0), dateFin) != 0) {
+        while (i < rows && strcmp(PQgetvalue(date_Debut, 1, 0), dateFin) != 0) {
             printf("Date : %s\n", strcmp(PQgetvalue(date_Debut, i, 0), dateFin) != 0);
             printf("Ligne : %d\n", rows);
             printf("i : %d\n", i);
