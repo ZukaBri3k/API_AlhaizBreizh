df0f3afe48b48e30469c7e9f839f99ba188f92b1
MathisLB <mathis.leboite@gmail.com>
2024-01-25 08:30:30 +0100

    Update read buffer size in client.c

--- client.c
+++ client.c
@@ -75,7 +75,7 @@ int main() {
             buffer[len] = '\0';
 
             while (len != 0) {
-                len = read(sock, buffer, sizeof(buffer));
+                len = read(sock, buffer, 1024);
                 printf("client : len = %d\n", len);
                 buffer[len] = '\0';
                 printf("%s", buffer);
