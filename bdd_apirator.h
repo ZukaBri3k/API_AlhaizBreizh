d0950c5ba311cb213ed277bdc18935ae6757fd11
EwanP56 <ewanpenven@gmail.com>
2024-04-02 17:20:56 +0200

    inchallah ça marche

--- bdd_apirator.h
+++ bdd_apirator.h
@@ -4,8 +4,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-extern void ecrireLogs(FILE *logs, char *message);
-extern bool verifCle(char input[15], FILE *logs);
-extern int getDispo(char input[15], int idLogement, char dateDebut[12], char dateFin[12], FILE *logs, FILE *json);
+extern void ecrireLogs(char *chemin, char *message);
+extern bool verifCle(char input[15], char *chemin);
+extern int getDispo(char input[15], int idLogement, char dateDebut[12], char dateFin[12], char *logs, FILE *json);
 
 #endif // BDD_APIRATOR_H
