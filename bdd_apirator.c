5864e88d6ed233197aeab42a1fc064989adfe09e
EwanP56 <ewanpenven@gmail.com>
2024-04-03 08:42:04 +0200

    inchallah ça marche

--- bdd_apirator.c
+++ bdd_apirator.c
@@ -232,14 +232,9 @@ int getDispo(char cle[15], int idLogement, char dateDebut[12], char dateFin[12],
                     fprintf(json, "\"");
                     fprintf(json, " : ");
                     fprintf(json, "true");
-                    fprintf(json, ",");
                     fprintf(json, "\n");
 
                     fprintf(json, "  }");
-
-                    if (i < num_days - 1) {
-                        fprintf(json, ",");
-                    }
                     fprintf(json, "\n");
                     fprintf(json, "");
                 }
